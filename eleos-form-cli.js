989d6a990ec922d241382fb50fa15f3b77935100
tfaulkner-eaglegroup <tfaulkner@eaglegroup.com>
2018-01-25 16:13:02 -0800

    Add keyfile

--- eleos-form-cli.js
+++ eleos-form-cli.js
@@ -1,49 +1,72 @@
 #!/usr/bin/env node
+const readlineSync = require('readline-sync')
+const request = require('request')
+const fs = require('fs')
+const crypto = require('crypto')
+const algo = 'aes-256-cbc'
+
 const argv = require('minimist')(process.argv.slice(2), {
   /* eslint-disable no-multi-spaces */
   boolean: [
-    'update',        'u',
-    'serialization', 's',
-    'visibility',    'v',
-    'noconfirm',     'n',
-    'overwrite',     'o'
+    'update',             'u',
+    'serialization-only', 's',
+    'visibility-only',    'v',
+    'no-confirm',         'n',
+    'overwrite',          'o'
   ]
   /* eslint-enable */
 })
 
-const readlineSync = require('readline-sync')
-const request = require('request')
-const fs = require('fs')
-
-const serverUrl = argv.overrideserver || 'https://platform.driveaxleapp.com/api/v1/forms/'
-const serializationOnly = argv.serialization || argv.s
-const visibilityOnly = argv.visibility || argv.v
-const noConfirm = argv.noconfirm || argv.n
+const serverUrl = argv['override-server'] || 'https://platform.driveaxleapp.com/api/v1/forms/'
+const serializationOnly = argv['serialization-only'] || argv.s
+const visibilityOnly = argv['visibility-only'] || argv.v
+const noConfirm = argv['no-confirm'] || argv.n
 const overwrite = argv.overwrite || argv.o
 const update = argv.update || argv.u
 const help = argv.help || argv.h
 
-const key = argv.key || argv._[0]
-const form = argv.form || argv._[1]
-const file = argv.file || argv._[2]
+const encryptionKey = argv.p || argv['password'] || '4RVo!SYwKaZXSqZ@CEQ2Nx'
+const keyfilePath = argv.keyfile || argv.k || process.env.Home + '.eleos-form-cli'
+const environment = argv.environment || argv.e || 'sandbox'
+const setKey = argv['set-key']
+
+const useKeyfile = setKey ? false : getKeyfile()
+const key = argv.key || argv._[0] || useKeyfile
+const form = argv.form || (useKeyfile ? argv._[0] : argv._[1])
+const file = argv.file || (useKeyfile ? argv._[1] : argv._[2])
+
+if (setKey && key) {
+  setKeyfile()
+  console.log(`Key set for ${setKey} environment`)
+  process.exit(0)
+}
 
 if (help || !key || !form || (serializationOnly && visibilityOnly)) {
   console.log('\n  Usage:')
-  console.log('  key form [file]            gets a form and either outputs to console or to file')
-  console.log('  key form file --update     sets a form using specified file')
+  console.log('  key form [file]                 gets a form and either outputs to console or to file')
+  console.log('  key form file --update          sets a form using specified file')
   console.log('\n  Flags:')
-  console.log('  --update          --u      sets a form or javascript property using the specified file')
-  console.log('  --serialiaztion   --s      gets/sets only the serialization_javascript property')
-  console.log('  --visibility      --v      gets/sets only the visibility_javascript property')
-  console.log('  --noconfirm       --n      skips confirmation prompts')
-  console.log('  --overwrite       --o      overwrites existing files')
-  console.log('  --overrideserver           server defaults to https://platform.driveaxleapp.com/api/v1/forms/')
+  console.log('  --update               --u      sets a form or javascript property using the specified file')
+  console.log('  --serialiaztion-only   --s      gets/sets only the serialization_javascript property')
+  console.log('  --visibility-only      --v      gets/sets only the visibility_javascript property')
+  console.log('  --no-confirm           --n      skips confirmation prompts')
+  console.log('  --overwrite            --o      overwrites existing files')
+  console.log('  --override-server               server defaults to https://platform.driveaxleapp.com/api/v1/forms/')
   console.log('\n  Cannot get/set both serialization and visibility at the same time.')
   console.log('\n  Examples:')
   console.log('      node eleos-form-editor.js SECRETKEY SomeFormCode')
   console.log('\n      eleos-form-editor SECRETKEY SomeFormCode serialization.js --update --serialization')
   console.log('\n  You can also specify named arguments instead of positional. For example:')
   console.log('      eleos-form-editor --key SECRETKEY --form SomeFormCode --file outfile.json')
+  console.log('\n  If your key begins with -- use = to assign argument value:')
+  console.log('      eleos-form-editor --key=--SECRETKEY')
+  console.log('\n  If you\'d like to save your key:')
+  console.log('      eleos-form-editor --save-key production --key SECRETKEY')
+  console.log('\n  Custom encryption key and file path: ')
+  console.log('      eleos-form-editor --password keyboardcat --keyfile ~/.keys --save-key sandbox --key SECRETKEY')
+  console.log('\n  Use --environment to set environment on run:')
+  console.log('      eleos-form-editor --environment production --password keyboardcat --form SomeForm')
+
   process.exit(1)
 }
 
@@ -57,6 +80,52 @@ const eleosRequest = {
 let fileContent = null
 if (file && update) {
   fileContent = fs.readFileSync(file, 'utf-8')
+}
+
+function getKeyfile () {
+  try {
+    const encrypted = JSON.parse(
+      fs.readFileSync(keyfilePath, 'utf-8')
+    )
+
+    const decipher = crypto.createDecipher(algo, encryptionKey)
+    let result = decipher.update(encrypted[environment], 'hex', 'utf8')
+    result += decipher.final('utf8')
+    return result
+  } catch (err) {
+    if (err.code === 'ENOENT') {
+      return false
+    } else {
+      throw err
+    }
+  }
+}
+
+function setKeyfile () {
+  let encrypted
+
+  try {
+    encrypted = JSON.parse(
+      fs.readFileSync(keyfilePath, 'utf-8')
+    )
+  } catch (err) {
+    if (err.code === 'ENOENT') {
+      encrypted = {}
+    } else {
+      throw err
+    }
+  }
+
+  const cipher = crypto.createCipher(algo, encryptionKey)
+  let crypted = cipher.update(key, 'utf-8', 'hex')
+  crypted += cipher.final('hex')
+  encrypted[setKey || environment] = crypted
+
+  fs.writeFileSync(
+    keyfilePath,
+    JSON.stringify(encrypted, null, 4),
+    { flag: 'w' }
+  )
 }
 
 function abort () {
